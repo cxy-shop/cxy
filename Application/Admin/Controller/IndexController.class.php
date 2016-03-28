5e7cec8dbd12bf68fc8bdbb13a3cabeb92e5554d
zhbitcxy <542610267@qq.com>
2016-03-29 07:59:58 +0800

    refactor

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,15 +2,16 @@
 namespace Admin\Controller;
 
 use Admin\Util\Menu;
-use Common\Controller\BaseController;
 
-class IndexController extends BaseController {
+class IndexController extends BaseController
+{
     /**
      * 后台管理主界面
      */
-    public function index(){
+    public function index()
+    {
         $menuUtil = new Menu();
-        $this->assign('menuStructure', $menuUtil->generate() );
+        $this->assign('menuStructure', $menuUtil->generate());
         $this->display();
     }
 
