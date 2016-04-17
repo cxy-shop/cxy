3314f8ab6028b64d587bedb6b757beedcc242b8c
zhbitcxy <542610267@qq.com>
2016-04-17 22:09:10 +0800

    add brand module

---
+++ Application/Admin/Controller/BrandController.class.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhbitcxy
+ * Date: 16/4/14
+ * Time: 下午10:34
+ */
+
+namespace Admin\Controller;
+use Admin\Model\BrandModel;
+
+/**
+ * 品牌管理
+ * Class BrandController
+ * @package Admin\Controller
+ */
+class BrandController extends BaseController
+{
+    /**
+     *  管理主界面
+     */
+    public function index()
+    {
+        $this->display();
+    }
+    /**
+     * 介绍页面
+     */
+    public function remark(){
+        $this->display();
+    }
+    /**
+     * 介绍页面
+     */
+    public function form(){
+        $this->display();
+    }
+
+    /**
+     * 获取列表
+     */
+    public function getList()
+    {
+        $cateId = I('cateId',0);
+        $brandService = new BrandModel();
+        $productUnitList = $brandService->scope('available')->where(['cate_id' => $cateId])->select();
+
+        $this->ajaxData($productUnitList);
+    }
+
+    /**
+     * 列表页面
+     */
+    public function lists()
+    {
+        $cateId = I('cateId', 0);
+        $this->assign('cateId', $cateId);
+        $this->display();
+    }
+
+    /**
+     * 新增操作
+     */
+    public function addHandle()
+    {
+        $brandService = new BrandModel();
+
+        $res = true;
+        $res = $brandService->create();
+        if ($res) {
+            $res = $brandService->add();
+        }
+        if ($res) {
+            $this->ajaxData( $brandService->find($res) );
+        } else {
+            $this->ajaxFail($brandService->getError());
+        }
+    }
+
+    /**
+     * 编辑操作
+     */
+    public function editHandle()
+    {
+        $brandService = new BrandModel();
+
+        $data = $brandService->create();
+        $res = null;
+        if ($data) {
+            $res = $brandService->save();
+        }
+        if ($res) {
+            $this->ajaxData($data);
+        } else {
+            $this->ajaxFail($brandService->getError());
+        }
+    }
+
+    /**
+     * 删除操作
+     */
+    public function removeHandle()
+    {
+        $id = I('id',0);
+        $brandService = new BrandModel();
+        $res = $brandService->delete($id);
+        if ($res) {
+            $this->ajaxSuccess();
+        } else {
+            $this->ajaxFail($brandService->getError());
+        }
+    }
+
+}
---
+++ Application/Admin/Model/BrandModel.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhbitcxy
+ * Date: 16/4/14
+ * Time: 下午10:43
+ */
+
+namespace Admin\Model;
+
+use Common\Model\XModel;
+
+class BrandModel extends XModel
+{
+    // 把表单字段映射到数据表的字段
+    protected $_map = [
+        'bannerImg'    =>  'banner_img',
+        'cateId'    =>  'cate_id',
+        'pcImg'    =>  'pc_img',
+    ];
+    //自动验证
+    protected $_validate = [
+        ['name', 'require', '名称必须！',self::MUST_VALIDATE], //验证必需字段
+        ['cate_id', 'require', '名称必须！',self::MUST_VALIDATE],
+        ['logo', 'require', 'logo必须！',self::EXISTS_VALIDATE ]
+    ];
+    //自动完成
+    protected $_auto = [
+        ['enable', 'toString', self::MODEL_BOTH, 'callback'], // 对name字段在新增和编辑的时候回调getName方法
+        ['recommend', 'toString', self::MODEL_BOTH, 'callback'], // 对name字段在新增和编辑的时候回调getName方法
+        ['last_edit_user_id', 'getCurrentUserId', self::MODEL_BOTH, 'callback'], // 对name字段在新增和编辑的时候回调getName方法
+        ['create_user_id', 'getCurrentUserId', self::MODEL_INSERT, 'callback'], // 对name字段在新增和编辑的时候回调getName方法
+        ['last_edit_user_id', 'getCurrentUserId', self::MODEL_BOTH, 'callback'], // 对name字段在新增和编辑的时候回调getName方法
+        ['create_time', 'getCurrentDatetime', self::MODEL_INSERT, 'callback'], // 对字段在新增的时候写入当前时间戳
+        ['last_edit_time', 'getCurrentDatetime', self::MODEL_BOTH, 'callback'], // 对字段在新增和更新的时候写入当前时间戳
+    ];
+    //命名范围
+    protected $_scope = [
+        'available' =>  []
+    ];
+
+    public function toString($value){
+        return strval($value);
+    }
+}
